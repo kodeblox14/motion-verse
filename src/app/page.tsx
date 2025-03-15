1f0643f1dc1e9c76000089b1daca8b6ed7579b3e
Khushbu Asati <khushbuasati1403@gmail.com>
2025-03-16 00:24:34 +0530

    initial commit setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="border border-solid border-amber-700 h-screen">
+
+    <h1 className="text-3xl font-bold underline bg-amber-400">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
